e6167076148883c236b7b52d8f803d60c914f17c
matias4205 <matiasperezpc@hotmail.com>
2019-08-27 23:46:16 -0300

    Now users service and schema added

---
+++ movies-api/services/users.js
@@ -0,0 +1,36 @@
+const Mongo = require('../lib/mongo');
+const bcrypt = require('bcrypt');
+
+class UsersService{
+    constructor(){
+        this.collection = 'users';
+        this.mongoDB = new Mongo();
+    }
+
+    async getUser(userId){
+        const query = query && {  };
+        const user = await this.mongoDB.get(this.collection, userId);
+        return user || {}
+    }
+
+    // async getUsers(query){
+    //     const query = query && {  };
+    //     const [ users ] = await this.mongoDB.getAll(this.collection, )
+    //     return users || [];
+    // }
+
+    async createUser(user){
+        const { name, email, password } = user;
+        const hashedPasswd = bcrypt.hash(password, 10);
+
+        const createdUserId = await this.mongoDB.create(this.collection, {
+            name,
+            email,
+            password: hashedPasswd
+        });
+
+        return createdUserId;
+    }
+}
+
+module.exports = UsersService;
